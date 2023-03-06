acef0e87fcb47dfd8d2b43ab120cf1d7b8c45873
ALOK1407007 <37080859+ALOK1407007@users.noreply.github.com>
2023-03-06 09:35:21 +0530

    nav-bar-1

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import STYLE from './app.module.css'
+const App = () => {
+  return (
+    
+    <div className={STYLE.a}>
+      
+      <div className={STYLE.a1}><img id={STYLE.im} src='https://logos-world.net/wp-content/uploads/2021/08/Byjus-Symbol.png'/></div>
+      <div className={STYLE.a21}>Study Material
+          <div className={STYLE.a212}>NCERT</div>
+          <div className={STYLE.a213}>NCERT</div>
+          <div className={STYLE.a214}>NCERT</div>
+      </div>
+      <div className={STYLE.a2}>Byjus Answer</div>
+      <div className={STYLE.a3}>Schlorship</div>
+      <div className={STYLE.a4}>BTC</div>
+      <div className={STYLE.a5}>Buy the coursse</div>
+      <div className={STYLE.a6}>Success Stories</div>
+      <div className={STYLE.a7}>Login</div>
+      
+    </div>
+
+  )
+}
+
+export default App
